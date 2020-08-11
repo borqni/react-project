a0d854b582ff17cdb5d3ea24a90be7a59eeff0a8
Boryana Georgieva <borqna_georgieva@abv.bg>
2020-08-11 08:52:17 +0300

    refactoring profile login pages

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -1,69 +1,58 @@
-import React, { Component } from 'react'
+import React, { useContext, useState, useEffect, useCallback } from 'react'
+import { useParams, useHistory } from 'react-router-dom'
 import styles from './index.module.css'
-import Submit from '../../components/submit-button'
 import Container from "../../components/container";
+import Submit from '../../components/submit-button'
 import Input from "../../components/input";
 import Pet from "../../components/pet";
 import UserContext from "../../Context";
 
-class Profile extends Component {
-    constructor(props) {
-        super(props)
+const Profile = () => {
+    const [username, setUsername] = useState(null)
+    const [pets, setPets] = useState(null)
+    const context = useContext(UserContext)
+    const params = useParams()
+    const history = useHistory()
 
-        this.state = {
-            username: null,
-            pets: null,
-            image: null
-        }
+    const logOut = () => {
+        context.logOut()
+        history.push('/')
     }
 
-    static contextType = UserContext
-
-    componentDidMount() {
-        this.getUser(this.props.match.params.userId)
-    }
-
-    getUser = async (id) => {
+    const getData = useCallback(async () => {
+        const id = params.userid
         const response = await fetch(`http://localhost:9999/api/user?id=${id}`)
 
         if (!response.ok) {
-            this.props.history.push('/error')
+            history.push('/error')
+        } else {
+            const user = await response.json()
+            setUsername(user.username)
+            setPets(user.pets && user.pets.length)
         }
+    }, [params.userid, history])
 
-        const user = await response.json()
+    useEffect(() => {
+        getData()
+    }, [getData])
 
-        this.setState = ({
-            username: user.username,
-            image: user.image,
-            pets: user.pets && user.pets.length
-        })
-    }
 
-    logout = () => {
-        this.context.logout()
-        this.props.history.push('/')
-    }
-
-    render() {
-        const { image, username, pets } = this.state
-
-        return (
-            <Container>
-                <div >
-                    <img src={image} />
-                    <div className={styles.profileInfo}>
-                        <p>Username: {username}</p>
-                        <p >Has {pets} pets </p>
-                        <div className={styles.myPets}>
-                            {/* pets.map(pet) */}
-                        </div>
+    return (
+        <Container>
+            <div >
+                {/* <img src={image} /> */}
+                <div className={styles.profileInfo}>
+                    <p>Username: {username}</p>
+                    <p >Has {pets} pets </p>
+                    <div className={styles.myPets}>
+                        {/* pets.map(pet) */}
                     </div>
-                    <button onClick={this.logout}>Logout</button>
                 </div>
-                <Pet />
-            </Container>
-        )
-    }
+                <button onClick={logOut}>Logout</button>
+            </div>
+            <Pet />
+        </Container>
+    )
 }
 
 export default Profile
